3f4d25f4bfb3a560a154bcc664979436674ac462
leonardo.murakami <leonardo.murakami@loft.com.br>
2020-04-04 10:23:02 -0300

    feat: added pages controllers to fit MVC archetype

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ const express = require('express'); // import express da maneira javascript
 const routes = require('./routes'); // import objeto routes da file routes.js (necessario o `./` que indica que estamos importando de uma file e não de uma biblioteca)
 
 const app = express(); // atribui o nosso aplicativo ao pacote express
+
+app.use(express.json());
+
 app.use(routes); // utilizar as rotas de routes
 
 app.listen(3333); // disponibiliza o aplicativo (quando rodado pelo node) na porta 3333
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,22 @@
 const express = require('express'); //import express
 
+const ongController = require('./controllers/ongsController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
 const routes = express.Router(); //estamos desacoplando o sistema de rotas do express para uma nova variavel
 
-routes.get('/', (request, response) => {
-    // return response.send('Hello World') <- returns text
-    const params = request.query;
-
-    return response.json({
-        name: 'Leonardo Heidi Almeida Murakami',
-        event: 'Participant at Omnistack 11.0 Week'
-    })
-}) // metodo que estava na file `index.js`
+// /sessions methods
+routes.post('/session', sessionController.create)
+// /ongs methods
+routes.post('/ongs', ongController.create);
+routes.get('/ongs', ongController.index);
+// /incidents methods
+routes.post('/incidents', incidentController.create);
+routes.get('/incidents', incidentController.index);
+routes.delete('/incidents/:id', incidentController.delete);
+// /profile methods
+routes.get('/profile', profileController.index);
 
 module.exports = routes; // variavel a ser exportada pelo script
