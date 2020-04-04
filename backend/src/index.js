11a33df816ad94eabaa2e67d547e0854dc7d2913
leonardo.murakami <leonardo.murakami@loft.com.br>
2020-04-04 10:58:34 -0300

    feat: added cors, joined ongs and incidents on incidentController

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express'); // import express da maneira javascript
+const cors = require('cors'); // import cors
 const routes = require('./routes'); // import objeto routes da file routes.js (necessario o `./` que indica que estamos importando de uma file e não de uma biblioteca)
 
 const app = express(); // atribui o nosso aplicativo ao pacote express
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes); // utilizar as rotas de routes
 
 app.listen(3333); // disponibiliza o aplicativo (quando rodado pelo node) na porta 3333
